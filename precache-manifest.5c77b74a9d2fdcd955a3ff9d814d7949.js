2dedc1f8b23d95864c06ec9783a75a3ef4a7b6e2
anshul-vandra <59761395+anshul-vandra@users.noreply.github.com>
2020-07-25 14:08:50 +0700

    Add files via upload

---
+++ precache-manifest.5c77b74a9d2fdcd955a3ff9d814d7949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eeeed29ff3cec389a81ea12d4d49e00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e798a682faa11dfa58eb",
+    "url": "/static/css/main.740d7ebc.chunk.css"
+  },
+  {
+    "revision": "f443086e4c6259dc8511",
+    "url": "/static/js/2.afa0af09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.afa0af09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e798a682faa11dfa58eb",
+    "url": "/static/js/main.66336cc1.chunk.js"
+  },
+  {
+    "revision": "fe2af3bd6869259f6b81",
+    "url": "/static/js/runtime-main.49925162.js"
+  }
+]);
